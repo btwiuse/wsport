d2fdeb10c3ac22dd6d2aeee4664d072ef3846a6b
navigaid <navigaid@gmail.com>
2024-05-05 15:31:59 +0000

    add cmd/http-proxy/streamHandler

---
+++ cmd/http-proxy/streamHandler.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	// We need to import libp2p's libraries that we use in this project.
+
+	"github.com/libp2p/go-libp2p/core/network"
+)
+
+// streamHandler is our function to handle any libp2p-net streams that belong
+// to our protocol. The streams should contain an HTTP request which we need
+// to parse, make on behalf of the original node, and then write the response
+// on the stream, before closing it.
+func streamHandler(stream network.Stream) {
+	// Remember to close the stream when we are done.
+	defer stream.Close()
+
+	// Create a new buffered reader, as ReadRequest needs one.
+	// The buffered reader reads from our stream, on which we
+	// have sent the HTTP request (see ServeHTTP())
+	buf := bufio.NewReader(stream)
+	// Read the HTTP request from the buffer
+	req, err := http.ReadRequest(buf)
+	if err != nil {
+		stream.Reset()
+		log.Println(err)
+		return
+	}
+	defer req.Body.Close()
+
+	// We need to reset these fields in the request
+	// URL as they are not maintained.
+	req.URL.Scheme = "http"
+	hp := strings.Split(req.Host, ":")
+	if len(hp) > 1 && hp[1] == "443" {
+		req.URL.Scheme = "https"
+	} else {
+		req.URL.Scheme = "http"
+	}
+	req.URL.Host = req.Host
+
+	outreq := new(http.Request)
+	*outreq = *req
+
+	// We now make the request
+	fmt.Printf("Making request to %s\n", req.URL)
+	resp, err := http.DefaultTransport.RoundTrip(outreq)
+	if err != nil {
+		stream.Reset()
+		log.Println(err)
+		return
+	}
+
+	// resp.Write writes whatever response we obtained for our
+	// request back to the stream.
+	resp.Write(stream)
+}
