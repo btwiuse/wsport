033eead196b55695e2d53c952adcb01495aed948
navigaid <navigaid@gmail.com>
2024-05-10 16:31:51 +0000

    add cmd/id example

--- cmd/go.mod
+++ cmd/go.mod
@@ -11,7 +11,9 @@ require (
 	github.com/btwiuse/gost v0.0.4
 	github.com/btwiuse/wsport v0.0.0
 	github.com/btwiuse/x-parity-wss v0.0.2
+	github.com/ipfs/go-datastore v0.6.0
 	github.com/libp2p/go-libp2p v0.33.2
+	github.com/libp2p/go-libp2p-kad-dht v0.25.2
 	github.com/multiformats/go-multiaddr v0.12.3
 	github.com/watjurk/ezlibp2p v0.0.0-20240330232927-17c7ded27593
 )
@@ -61,7 +63,6 @@ require (
 	github.com/huin/goupnp v1.3.0 // indirect
 	github.com/ipfs/boxo v0.10.0 // indirect
 	github.com/ipfs/go-cid v0.4.1 // indirect
-	github.com/ipfs/go-datastore v0.6.0 // indirect
 	github.com/ipfs/go-log v1.0.5 // indirect
 	github.com/ipfs/go-log/v2 v2.5.1 // indirect
 	github.com/ipld/go-ipld-prime v0.20.0 // indirect
@@ -76,7 +77,6 @@ require (
 	github.com/libp2p/go-cidranger v1.1.0 // indirect
 	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-asn-util v0.4.1 // indirect
-	github.com/libp2p/go-libp2p-kad-dht v0.25.2 // indirect
 	github.com/libp2p/go-libp2p-kbucket v0.6.3 // indirect
 	github.com/libp2p/go-libp2p-record v0.2.0 // indirect
 	github.com/libp2p/go-libp2p-routing-helpers v0.7.2 // indirect
---
+++ cmd/id/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	// We need to import libp2p's libraries that we use in this project.
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+
+	"github.com/btwiuse/wsport"
+)
+
+func getEnv(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+func getPrivKey() (crypto.PrivKey, error) {
+	if pk := getEnv("PRIV_KEY", ""); pk != "" {
+		keyBytes, err := crypto.ConfigDecodeKey(pk)
+		if err != nil {
+			return nil, err
+		}
+		return crypto.UnmarshalPrivateKey(keyBytes)
+	}
+
+	privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return privKey, nil
+}
+
+var RELAY = getEnv("RELAY", "https://example.com")
+
+// makeRandomHost creates a libp2p host with a randomly generated identity.
+// This step is described in depth in other tutorials.
+func makeRandomHost(port int) host.Host {
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+	privKey, _ := getPrivKey()
+	addr := fmt.Sprintf(RELAY+"/ws%d", port)
+	maddr, _ := wsport.FromString(addr)
+	peerID, _ := peer.IDFromPublicKey(privKey.GetPublic())
+	maddr = maddr.Encapsulate(ma.StringCast("/p2p/" + peerID.String()))
+	slog.Info("Listen", "addr", addr, "maddr", maddr)
+	host, err := libp2p.New(
+		libp2p.Transport(wsport.New),
+		wsport.ListenAddrStrings(addr),
+		// libp2p.ListenAddrs(maddr),
+		libp2p.Identity(privKey),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return host
+}
+
+func main() {
+	host := makeRandomHost(1000)
+	fmt.Println("host is ready")
+	fmt.Println("libp2p-peer addresses:")
+	for _, a := range host.Addrs() {
+		fmt.Printf("%s/p2p/%s\n", a, host.ID())
+	}
+	select {}
+}
