5f9c82be698d84a61e2e60242bee800f5baf17ce
navigaid <navigaid@gmail.com>
2024-05-10 19:57:05 +0000

    peerid: load from disk: libp2p_id

--- cmd/peerid/main.go
+++ cmd/peerid/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
 	"log"
 	"log/slog"
@@ -11,13 +10,11 @@ import (
 	// We need to import libp2p's libraries that we use in this project.
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/routing"
-	ma "github.com/multiformats/go-multiaddr"
 
 	"github.com/btwiuse/wsport"
+	"github.com/watjurk/ezlibp2p"
 )
 
 func getEnv(key, def string) string {
@@ -27,40 +24,24 @@ func getEnv(key, def string) string {
 	return def
 }
 
-func getPrivKey() (crypto.PrivKey, error) {
-	if pk := getEnv("PRIV_KEY", ""); pk != "" {
-		keyBytes, err := crypto.ConfigDecodeKey(pk)
-		if err != nil {
-			return nil, err
-		}
-		return crypto.UnmarshalPrivateKey(keyBytes)
-	}
-
-	privKey, _, err := crypto.GenerateEd25519Key(rand.Reader)
-	if err != nil {
-		return nil, err
-	}
-
-	return privKey, nil
-}
-
 var RELAY = getEnv("RELAY", "https://example.com")
 
 // makeRandomHost creates a libp2p host with a randomly generated identity.
 // This step is described in depth in other tutorials.
 func makeRandomHost(port int) host.Host {
 	log.SetFlags(log.LstdFlags | log.Llongfile)
-	privKey, _ := getPrivKey()
 	addr := fmt.Sprintf(RELAY+"/ws%d", port)
-	maddr, _ := wsport.FromString(addr)
-	peerID, _ := peer.IDFromPublicKey(privKey.GetPublic())
-	maddr = maddr.Encapsulate(ma.StringCast("/p2p/" + peerID.String()))
-	slog.Info("Listen", "addr", addr, "maddr", maddr)
+	slog.Info("Listen", "addr", addr)
+
+	identity, err := ezlibp2p.PersistentIdentity()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	host, err := libp2p.New(
 		libp2p.Transport(wsport.New),
 		wsport.ListenAddrStrings(addr),
-		// libp2p.ListenAddrs(maddr),
-		libp2p.Identity(privKey),
+		identity,
 		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
 			return dht.New(
 				context.Background(),
