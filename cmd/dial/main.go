8acac58230d92ac4365f722e5da6fb3c85d57373
navigaid <navigaid@gmail.com>
2024-05-22 18:55:16 +0000

    cmd/dial: support dialing multiaddr without peer id
    https://github.com/libp2p/go-libp2p/issues/1040

--- cmd/dial/main.go
+++ cmd/dial/main.go
@@ -8,8 +8,12 @@ import (
 
 	// We need to import libp2p's libraries that we use in this project.
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
+	tptu "github.com/libp2p/go-libp2p/p2p/net/upgrader"
+	"github.com/libp2p/go-libp2p/p2p/security/noise"
+	ma "github.com/multiformats/go-multiaddr"
 
 	"github.com/btwiuse/wsport"
 )
@@ -32,6 +36,21 @@ func makeRandomHost(port int) host.Host {
 	host, err := libp2p.New(
 		libp2p.Transport(wsport.New),
 		wsport.ListenAddrStrings(addr),
+		// it is a failed attempt to disable peer id check
+		// taken from https://github.com/quochoandkh/go-spacemesh/blob/a06f7e8eff9fa2ce7d9bccdc15e8b40b21495c3b/p2p/host.go#L222
+		// safe to delete later
+		libp2p.Security(
+			noise.ID,
+			func(id protocol.ID, privkey crypto.PrivKey, muxers []tptu.StreamMuxer) (*noise.SessionTransport, error) {
+				tp, err := noise.New(id, privkey, muxers)
+				if err != nil {
+					return nil, err
+				}
+				return tp.WithSessionOptions(
+				// noise.DisablePeerIDCheck(),
+				)
+			},
+		),
 	)
 	if err != nil {
 		log.Fatalln(err)
@@ -47,7 +66,12 @@ func main() {
 		fmt.Printf("%s/p2p/%s\n", a, host.ID())
 	}
 
-	addrInfo, err := peer.AddrInfoFromString(os.Args[1])
+	maddr, err := ma.NewMultiaddr(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	addrInfo, err := AddrInfo(maddr)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -56,7 +80,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	} else {
-		fmt.Println("Connected to", addrInfo.ID)
+		fmt.Println("Connected to", addrInfo)
 	}
 
 	select {}
